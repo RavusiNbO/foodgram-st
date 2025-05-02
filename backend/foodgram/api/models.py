71c5d2df24955d0a9acfcd16c8769781fd4408f3
RavusiNbO <ravaravus@vk.com>
2025-05-02 11:27:52 +0300

    pagination

--- backend/foodgram/api/models.py
+++ backend/foodgram/api/models.py
@@ -4,10 +4,14 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-#adsd
+
 class Ingredient(models.Model):
     name = models.CharField("Название", max_length=100, null=False)
-    measurement_unit = models.CharField("Единица измерения", max_length=15, null=False)
+    measurement_unit = models.CharField(
+        "Единица измерения",
+        max_length=15,
+        null=False
+    )
 
     class Meta:
         verbose_name = "Ингредиент"
@@ -18,10 +22,19 @@ class Ingredient(models.Model):
 
 
 class Recipe(models.Model):
-    name = models.CharField("Название", max_length=256, null=False, blank=False)
+    name = models.CharField(
+        "Название",
+        max_length=256,
+        null=False,
+        blank=False
+    )
     image = models.ImageField("Картинка", null=False, blank=False)
     text = models.TextField("Описание", null=False, blank=False)
-    cooking_time = models.IntegerField("Время приготовления", null=False, blank=False)
+    cooking_time = models.IntegerField(
+        "Время приготовления",
+        null=False,
+        blank=False
+    )
     ingredients = models.ManyToManyField(
         Ingredient, verbose_name="Ингредиенты", blank=False, through="Amount"
     )
@@ -49,5 +62,9 @@ class Amount(models.Model):
         verbose_name="Ингредиент",
         related_name="amount",
     )
-    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, verbose_name="Рецепт")
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        verbose_name="Рецепт"
+    )
     amount = models.IntegerField("Вес", null=False, blank=False)
