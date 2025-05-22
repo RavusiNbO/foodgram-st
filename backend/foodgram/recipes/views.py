a2cb9f1152b816731b1c4097d2ddcf511628763d
RavusiNbO <ravaravus@vk.com>
2025-05-22 14:30:58 +0300

    asdasd

--- backend/foodgram/recipes/views.py
+++ backend/foodgram/recipes/views.py
@@ -5,7 +5,9 @@ from rest_framework import status
 
 @api_view(["GET"])
 def get_short_link(request, pk=None):
-    res = request.build_absolute_uri().split("get-link")[0]
+    res = ''.join(
+        request.build_absolute_uri().split("get-link")[0].split('/api'))
+
     return Response({
         "short-link": res,
     }, status=status.HTTP_200_OK)
